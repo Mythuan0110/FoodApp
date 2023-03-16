b72ff59693a501af2ea1038f3fd3d54249eb7235
THUAN <THUAN@gmail.com>
2023-03-16 09:08:56 +0700

    mythuanfoodapp

---
+++ Food-master/app/src/main/java/com/example/food/interfaceI/Storecallback.java
@@ -0,0 +1,10 @@
+package com.example.food.interfaceI;
+
+import com.example.food.model.Store;
+
+import java.util.ArrayList;
+
+public interface Storecallback {
+    void onSuccess(ArrayList<Store> lists);
+    void onError(String message);
+}
